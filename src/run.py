e5f2e52091c5675c73987d4e48116aef19ed11bd
guillermochica <1bgchica@gmail.com>
2015-08-17 13:07:48 +0200

    Added support for time

--- src/run.py
+++ src/run.py
@@ -2,7 +2,7 @@
 import sys
 import lib.Temperature as Temperature
 import lib.Humidity as Humidity
-
+import lib.Clock as Clock
 
 STATES={'T': 'Temperature',
 		'H': 'Humidity',
@@ -10,16 +10,20 @@ STATES={'T': 'Temperature',
 if len(sys.argv) == 2 and sys.argv[1] in STATES:
 	current_state = STATES[sys.argv[1]]
 else:
-	print 'To use it write: sudo ./run [T|H]'
+	print 'To use it write: sudo ./run [T|H|C]'
 
 try:
 	if current_state == 'Temperature':
 		Temperature.runTemperature()
 	elif current_state == 'Humidity':
 		Humidity.runHumidity()
+	elif current_state == 'Clock':
+		Clock.runClock()
 except KeyboardInterrupt:
 	print '\nGoodbye...'
 	if current_state == 'Temperature':
 		Temperature.segment.__init__()
 	elif current_state == 'Humidity':
 		Humidity.segment.__init__()
+	elif current_state == 'Clock':
+		Clock.segment.__init__()
